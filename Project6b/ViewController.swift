a656f18745a5d59a68547543e101048c77398dd5
philip713 <105677806+philip713@users.noreply.github.com>
2023-08-22 10:23:33 -0600

    Initial Commit

---
+++ Project6b/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6b
+//
+//  Created by Philip Janzel Paradeza on 2023-08-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
